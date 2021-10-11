4aab8f8ec8933e1f71562d8e8bd20c5f099d8a87
Julian 41455100+thieleju@users.noreply.github.com <thiele.juli@gmail.com>
2021-10-11 15:47:01 +0200

    added function

--- Hello_World/Main.java
+++ Hello_World/Main.java
@@ -6,9 +6,11 @@ public class Main {
 
   public static void main(String[] args) {
 
-    Scanner s = new Scanner(System.in);
-    String name = s.nextLine();
+    System.out.println("hello " + getInput());
+  }
 
-    System.out.println("hello " + name);
+  public static String getInput() {
+    Scanner s = new Scanner(System.in);
+    return s.nextLine();
   }
 }
