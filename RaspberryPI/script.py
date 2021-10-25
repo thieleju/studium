1bbc7734e1fa40c0944dfcc1460664e9c9cbfc2c
Julian 41455100+thieleju@users.noreply.github.com <thiele.juli@gmail.com>
2021-10-25 19:28:35 +0200

    added rasp script

---
+++ RaspberryPI/script.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+import time
+
+# set mode to boardnumbers or GPIO number
+# GPIO number
+GPIO.setmode(GPIO.BCM)
+
+pin = input("Choose output bin to blink")
+
+iterations = input("How many times?")
+
+GPIO.setup(pin, GPIO.OUT)
+
+for i in range(itarations):
+	GPIO.output(pin, GPIO.HIGH)
+	time.sleep(0.5)
+	GPIO.output(pin, GPIO.LOW)
+	time.sleep(0.5)
