b62566a9d4683c4d6c83171d9518cfb93096396f
Julian 41455100+thieleju@users.noreply.github.com <thiele.juli@gmail.com>
2021-11-25 09:49:30 +0100

    Add runterzählen funktion

---
+++ RaspberryPI/explorerhat/Aufgabe7.py
@@ -0,0 +1,65 @@
+import time
+import explorerhat as hat
+
+
+def nametonumber(name):
+    if name == "one":
+        return 0
+    if name == "two":
+        return 1
+    if name == "three":
+        return 2
+    if name == "four":
+        return 3
+    else:
+        return -1
+
+
+# Reset auf Low setzen
+hat.output[3].on()
+
+# PL auf High
+hat.output[2].off()
+
+
+def evaluateButtons(channel, event):
+    # count up: high = output off
+    #   CPD = high
+    #   CPU = output 0 (low to high)
+
+    # count down: high = output off
+    #   CPD = output 1 (low to high)
+    #   CPU = high
+
+    # hochzählen, CPU flanke, CPD auf High
+    if channel == 6 and event == 'press':
+        hat.output[0].on()
+        hat.output[1].off()
+    if channel == 6 and event == 'release':
+        hat.output[0].off()
+
+    # runter zählen
+    if channel == 7 and event == 'press':
+        hat.output[1].on()
+        hat.output[0].off()
+    if channel == 7 and event == 'release':
+        hat.output[0].off()
+
+    # flanke für reset
+    if channel == 8 and event == 'press':
+        hat.output[3].off()
+    if channel == 8 and event == 'release':
+        hat.output[3].on()
+
+
+def evaluateInput(input):
+    global binArray
+    number = nametonumber(input.name)
+    hat.light[(number-3)*(-1)].write(input.read())
+    print("Input {} changed to {}".format(input.name, input.read()))
+
+
+hat.input.on_changed(evaluateInput)
+hat.touch.pressed(evaluateButtons)
+hat.touch.released(evaluateButtons)
+hat.pause()
