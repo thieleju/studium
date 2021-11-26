eb39917c561823a046b50bba16d6ded387d27ab7
Julian <thiele.juli@gmail.com>
2021-11-26 22:20:22 +0100

    Update Aufgabe 8 no button yet

--- RaspberryPI/explorerhat/Aufgabe8.py
+++ RaspberryPI/explorerhat/Aufgabe8.py
@@ -1,82 +1,77 @@
 import time
 import explorerhat as hat
 
-print("""SD_GIT_2122_U08
-Ampelschaltung""")
+print("""SD_GIT_2122_U08 Ampelschaltung""")
 
-# Eine Hilfsfunktion, die uns unsere Zustandsnamen auf Codierungen abbildet
-# Hier ist nichts zu verändern, es sei denn, Sie verwenden eine andere
-# Kodierung
+# !!!! Zustände wurden geändert !!!!
 
 
 def stateToCode(name):
-    if name == "rot":
+    if name == "gruen":
         return [0, 0]
 
-    if name == "rotgelb":
+    if name == "rot":
         return [0, 1]
 
-    if name == "gruen":
+    if name == "gelb":
         return [1, 0]
 
-    if name == "gelb":
+    if name == "rotgelb":
         return [1, 1]
-
-    else:
-        return [0, 0]
 
 
 # Der initiale Zustand: Die Ampel ist auf rot
 state = "rot"
 
-# Die Zeit, die zwischen Umschaltvorgängen vergeht
-waitingtime = 3
+# Die Zeit, die zwischen Umschaltvorgaengen vergeht
+waitingtime = 2
 
-# Anforderungskontakt gedrückt oder nicht
+# Anforderungskontakt gedrueckt oder nicht
 pedestrian_req = 0
 
 # der Handler, um einen Druck auf die Anforderungstaste entgegenzunehmen
 
 
 def pedreq(channel, event):
-    # wenn Taste 1 gedrückt, setzen wir pedestrian_req
+    # wenn Taste 1 gedrueckt, setzen wir pedestrian_req
+    print(channel, event)
 
-    # Registrierung des Handlers, der die Fußgängeranforderung entgegennimmt
+
+# Registrierung des Handlers, der die Fussgaengeranforderung entgegennimmt
 hat.touch.pressed(pedreq)
 
-
 while 1:
-    # zu Beginn jedes Durchlaufs wird der aktuelle Zustand nach
-    # außen gegeben. Da output.on() bewirkt, dass der Pin auf
-    # GND (also die logische 0) gezogen wird, invertieren wir
-    # das jeweilige Bit der Zustandskodierung
-    # Hier sollten Sie nichts verändern
-    hat.output[1].write(not stateToCode(state)[1])
-    hat.output[0].write(not stateToCode(state)[0])
+    hat.output[1].write(stateToCode(state)[1])
+    hat.output[0].write(stateToCode(state)[0])
 
-    # Die eigentliche Zustandsmaschine, die basierend auf aktuellem Zustand
-    # und optionalem Eingang entscheidet, was der Folgezustand sein wird
+    print("code ist ", stateToCode(state))
+
     if state == "rot":
-        # für Ihre Fehlersuche ggf. hilfreich
-        print("code ist {}".format(stateToCode(state)))
-
         # Onboard LEDs des Explorer HAT ein- und ausschalten
         hat.light.red.on()
         hat.light.yellow.off()
         hat.light.green.off()
 
-        # Hier prüfen wir die Fußgägngeranforderung und schalten
-        # die Fußgängerampel ggf. auf grün (blau)
+        # Hier pruefen wir die Fussgaegngeranforderung und schalten
+        # die Fussgaengerampel ggf. auf gruen (blau)
         # if ...
 
-        # durch welche Zuweisung wechseln Sie in den nächsten Zustand?
-        #state = ...
+        # durch welche Zuweisung wechseln Sie in den naechsten Zustand?
+        state = "rotgelb"
+    elif state == "rotgelb":
+        hat.light.red.on()
+        hat.light.yellow.on()
+        hat.light.green.off()
+        state = "gruen"
+    elif state == "gruen":
+        hat.light.red.off()
+        hat.light.yellow.off()
+        hat.light.green.on()
+        state = "gelb"
+    elif state == "gelb":
+        hat.light.red.off()
+        hat.light.yellow.on()
+        hat.light.green.off()
+        state = "rot"
 
-        # ein wenig Wartezeit vor nächster Phase
-        time.sleep(waitingtime*2)
-
-    # Hier müssen Sie den Zustandsautomaten ergänzen
-    # warum nehmen wir elif und nicht einfach ein neues if?
-
-    # elif state == ...
-    # ...
+time.sleep(waitingtime)
