abefb59cdfefcb3466d4b289c65c04954a665d4b
Julian 41455100+thieleju@users.noreply.github.com <thiele.juli@gmail.com>
2022-05-19 18:13:40 +0200

    Added workbook 4

---
+++ Programmiertechnik_II/Workbook4/Main.java
@@ -0,0 +1,40 @@
+package Programmiertechnik_II.Workbook4;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    // 1
+    JFrame myFrame = new JFrame();
+    myFrame.setSize(350, 350);
+    myFrame.setTitle("Mein erstes Swing-Fenster!");
+    // Diese Anweisung zentriert die Anzeige des Fensters
+    myFrame.setLocationRelativeTo(null);
+    // Diese Anweisung schliesst das Fenster, wenn Exit betätigt wird
+    myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    JButton myButton = new JButton("Button");
+    myButton.setToolTipText("OK");
+
+    myFrame.add(myButton);
+    myFrame.setVisible(true);
+
+    // 2
+    // Welche Listener-Schnittstelle wird hier implementiert?
+    // ActionListener?
+
+    // In welcher Zeile wird der EventHandler implementiert?
+    // 6
+
+    // Welche Eventquelle wird abgehört? Woran sieht man das im Code?
+    // myButtonClickMich
+
+    new EinfacheGui();
+
+    // *Letzte Aufgabe*
+
+  }
+}
