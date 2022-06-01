2e837f724016f9860e9d9f31f1ea7c82088b1d3a
Julian <thiele.juli@gmail.com>
2022-06-01 20:03:06 +0200

    Aufgabe 1 und 2

---
+++ Programmiertechnik_II/Workbook6/Main.java
@@ -0,0 +1,30 @@
+package Programmiertechnik_II.Workbook6;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    // 1
+    // Variablen einer inneren Klasse können nicht in der äußeren Klasse verwendet
+    // werden. Innere Klassen können alle Variablen und Funktionen der äußeren
+    // Klasse verwendet werden
+
+    // Funktionen einer inneren Klasse können von äußen nicht aufgerufen werden
+
+    // Die Funktion der innneren Klasse kann von der äußeren Klasse aufgerufen
+    // werden
+    // Huelle h = new Huelle();
+    // h.machWasInDerHuelle();
+    // h.aussenMethode();
+
+    // Es kann von der äußeren Klasse auch auf private Attribute der inneren Klasse
+    // zugegriffen werden
+
+    // 2
+
+    Animation a = new Animation();
+    a.startAnimation(2);
+
+  }
+
+}
