cbf7ac6f4787dd2c401684ab535d39edb4c27504
Julian <thiele.juli@gmail.com>
2022-07-08 22:31:03 +0200

    Kata 6 - Interface

---
+++ Programmiertechnik_II/Kata6/CanyonMountainbike.java
@@ -0,0 +1,9 @@
+public class CanyonMountainbike extends Mountainbike {
+
+  @Override
+  public void fahren() {
+    // TODO Auto-generated method stub
+
+  }
+
+}
---
+++ Programmiertechnik_II/Kata6/IAbschliessbar.java
@@ -0,0 +1,3 @@
+public interface IAbschliessbar {
+  public void abschliessen();
+}
