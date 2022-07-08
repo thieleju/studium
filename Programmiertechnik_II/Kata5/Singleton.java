57ac5b635540bd69ae6b4c8a1d0b5e177d9fee44
Julian 41455100+thieleju@users.noreply.github.com <thiele.juli@gmail.com>
2022-07-08 22:25:05 +0200

    Added Kata 5 - Singleton

---
+++ Programmiertechnik_II/Kata5/Singleton.java
@@ -0,0 +1,12 @@
+public class Singleton {
+
+  private static Singleton instance = null;
+
+  public static Singleton getInstance() {
+    if (instance == null)
+      instance = new Singleton();
+
+    return instance;
+  }
+
+}
