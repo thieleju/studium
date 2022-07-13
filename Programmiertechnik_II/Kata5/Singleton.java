2114233d292d14fc21684ad40dfa1c6927b4837f
Julian <thiele.juli@gmail.com>
2022-07-13 09:16:03 +0200

    Updated Sinlgleton

--- Programmiertechnik_II/Kata5/Singleton.java
+++ Programmiertechnik_II/Kata5/Singleton.java
@@ -1,11 +1,11 @@
 public class Singleton {
 
-  private static Singleton instance = null;
+  private static Singleton instance = new Singleton();
+
+  private Singleton() {
+  }
 
   public static Singleton getInstance() {
-    if (instance == null)
-      instance = new Singleton();
-
     return instance;
   }
 
