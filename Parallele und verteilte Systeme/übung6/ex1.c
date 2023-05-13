33f5a20107ac3f6b0af97aa38771d5744722b4c2
Julian 41455100+thieleju@users.noreply.github.com <thiele.juli@gmail.com>
2023-05-13 16:05:50 +0200

    Refactored prime number generation and finished ex

--- Parallele und verteilte Systeme/übung6/ex1.c
+++ Parallele und verteilte Systeme/übung6/ex1.c
@@ -2,57 +2,141 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/resource.h>
+#include <math.h>
 
-void generate_primes(int start, int end, char* label) {
-    // Generate prime numbers in the range [start, end] and print them
-    int is_prime;
-    for (int n = start; n <= end; n++) {
-        is_prime = 1;
-        for (int i = 2; i < n; i++) {
-            if (n % i == 0) {
-                is_prime = 0;
-                break;
-            }
-        }
-        if (is_prime) {
-            printf("%s: Prime %d\n", label, n);
-        }
+void generate_primes_fast(int n)
+{
+  int *primes;
+  primes = (int *)malloc(n * sizeof(int));
+
+  if (primes == NULL)
+  {
+    printf("Error: malloc() failed\n");
+    exit(EXIT_FAILURE);
+  }
+
+  primes[0] = 2;
+
+  int found_primes = 1;
+  int current = 3;
+  int is_prime;
+
+  while (found_primes != n)
+  {
+    is_prime = 1;
+    for (int i = 0; i < sqrt(found_primes); i++)
+    {
+      if (current % primes[i] == 0)
+      {
+        is_prime = 0;
+        break;
+      }
     }
+    if (is_prime)
+    {
+      primes[found_primes++] = current;
+    }
+    current += 2;
+  }
+  printf("-> Largest prime: %i (fast)\n", primes[n - 1]);
+  free(primes);
+  primes = NULL;
 }
 
-int main() {
-    int pid1, pid2;
-    int status;
+void generate_primes_slow(int n)
+{
+  int *primes;
+  primes = (int *)malloc(n * sizeof(int));
+  if (primes == NULL)
+  {
+    printf("Error: malloc() failed\n");
+    exit(EXIT_FAILURE);
+  }
 
-    // Fork the first child process
-    pid1 = fork();
-    if (pid1 == -1) {
-        printf("Error: fork() failed\n");
-        exit(1);
-    } else if (pid1 == 0) {
-        // This is the first child process
-        printf("First child process (pid=%d) created\n", getpid());
-        generate_primes(2, 1000, "First child");
-        exit(0);
+  primes[0] = 2;
+
+  int found_primes = 1;
+  int current = 3;
+  int is_prime;
+
+  while (found_primes != n)
+  {
+    is_prime = 1;
+    for (int i = 0; i < found_primes / 2; i++)
+    {
+      if (current % primes[i] == 0)
+      {
+        is_prime = 0;
+        break;
+      }
     }
+    if (is_prime)
+    {
+      primes[found_primes++] = current;
+    }
+    current += 2;
+  }
+  printf("-> Largest prime: %i (slow)\n", primes[n - 1]);
+  free(primes);
+  primes = NULL;
+}
 
-    // Fork the second child process
+double get_time()
+{
+  struct rusage usage;
+  getrusage(RUSAGE_SELF, &usage);
+  return usage.ru_utime.tv_sec + (usage.ru_utime.tv_usec / 1000000.0);
+}
+
+int main()
+{
+  pid_t pid1, pid2;
+  int status;
+
+  pid1 = fork();
+
+  if (pid1 == -1)
+  {
+    perror("fork");
+    exit(EXIT_FAILURE);
+  }
+  else if (pid1 == 0)
+  { // child process 1
+    double start_time = get_time();
+    generate_primes_fast(50000);
+    double end_time = get_time();
+
+    printf("\nFast CPU time: %f seconds\n\n", end_time - start_time);
+    exit(EXIT_SUCCESS);
+  }
+  else
+  {
+    // parent process
     pid2 = fork();
-    if (pid2 == -1) {
-        printf("Error: fork() failed\n");
-        exit(1);
-    } else if (pid2 == 0) {
-        // This is the second child process
-        printf("Second child process (pid=%d) created\n", getpid());
-        generate_primes(1001, 2000, "Second child");
-        exit(0);
+    if (pid2 == -1)
+    {
+      perror("fork");
+      exit(EXIT_FAILURE);
     }
+    else if (pid2 == 0)
+    { // child process 2
+      double start_time = get_time();
+      generate_primes_slow(50000);
+      double end_time = get_time();
 
-    // Wait for both child processes to finish
-    waitpid(pid1, &status, 0);
-    printf("First child process (pid=%d) terminated with status %d\n", pid1, status);
-    waitpid(pid2, &status, 0);
-    printf("Second child process (pid=%d) terminated with status %d\n", pid2, status);
+      printf("\nSlow CPU time: %f seconds\n\n", end_time - start_time);
 
-    return 0;
+      exit(EXIT_SUCCESS);
+    }
+    else
+    { // parent process
+      waitpid(pid1, &status, 0);
+      waitpid(pid2, &status, 0);
+
+      printf("\nBoth processes finished.\n\n");
+    }
+  }
+
+  return 0;
 }
